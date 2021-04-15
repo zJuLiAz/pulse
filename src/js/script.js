93588b27b72b2c85263e9f14a6577d1b818b518e
juliawebscat <juliawebscat@gmail.com>
2021-04-16 00:48:44 +0300

    сделана прокрутка вверх и анимация сердца

--- src/js/script.js
+++ src/js/script.js
@@ -98,5 +98,21 @@ $(document).ready(function(){
             $('form').trigger('reset');
         });
         return false;
-    })
+    });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
